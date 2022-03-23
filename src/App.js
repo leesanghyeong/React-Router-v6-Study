62b8296d84517fb707d62fcc8ed03326a9c9a1fe
LeeSang <dltkdgud317@gmail.com>
2022-03-23 13:42:23 +0900

    리액스 라우터 돔 v6 <Switch> -> <Routes>로 변경된점 학습

--- src/App.js
+++ src/App.js
@@ -1,37 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-import { Col, Row } from 'reactstrap';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <>
-    <Row>
-      <Col>test</Col>
-      <Col>test2</Col>
-      <Col>test3</Col>
-    </Row>
-    <Row>
-      <Col>test</Col>
-      <Col>test2</Col>
-    </Row>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/about' element={<About />} />
+          <Route path='/users' element={<Users />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+ return (
+   <div>
+     <h2>Home</h2>
+   </div>
+ );
+}
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+function Users() {
+  return (
+    <div>
+      <h2>Users</h2>
+    </div>
+  );
+}
